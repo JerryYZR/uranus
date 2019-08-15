ba2260942954ee0b31a8870194f548f89785e6ba
28877110 <441423487@qq.com>
2019-08-15 13:54:24 +0800

    Merge branch 'zsd' of https://github.com/JerryYZR/uranus
    # Conflicts:
    #	pom.xml

--- src/main/java/com/team7/uranus/aspect/SystemLogAspect.java
+++ src/main/java/com/team7/uranus/aspect/SystemLogAspect.java
@@ -7,28 +7,41 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Modifier;
+
 @Slf4j
 @Component
 @Aspect
 public class SystemLogAspect {
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
-    public void pointcut(JoinPoint point) {
+    public void pointcut() {
 
     }
 
-//    @After("pointcut()")
-//    public void doAfter(JoinPoint point) throws Throwable {
-//        log.info("请求了接口。");
-//    }
+    @After("pointcut()")
+    public void doAfter(JoinPoint joinPoint) throws Throwable {
+        log.info("目标方法名为:" + joinPoint.getSignature().getName());
+        log.info("目标方法所属类的简单类名:" +        joinPoint.getSignature().getDeclaringType().getSimpleName());
+        log.info("目标方法所属类的类名:" + joinPoint.getSignature().getDeclaringTypeName());
+        log.info("目标方法声明类型:" + Modifier.toString(joinPoint.getSignature().getModifiers()));
+        //获取传入目标方法的参数
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            log.info("第" + (i+1) + "个参数为:" + args[i]);
+        }
+        log.info("被代理的对象:" + joinPoint.getTarget());
+        log.info("代理对象自己:" + joinPoint.getThis());
+        log.info("请求了接口。");
+    }
 //
 //
 //    @After("execution(* com.abc.service.*.many*(..))")
 //    public void releaseResource(JoinPoint point) {
-//        System.out.println("@After：模拟释放资源...");
-//        System.out.println("@After：目标方法为：" +
+//        log.info("@After：模拟释放资源...");
+//        log.info("@After：目标方法为：" +
 //                point.getSignature().getDeclaringTypeName() +
 //                "." + point.getSignature().getName());
-//        System.out.println("@After：参数为：" + Arrays.toString(point.getArgs()));
-//        System.out.println("@After：被织入的目标对象为：" + point.getTarget());
+//        log.info("@After：参数为：" + Arrays.toString(point.getArgs()));
+//        log.info("@After：被织入的目标对象为：" + point.getTarget());
 //    }
 }
