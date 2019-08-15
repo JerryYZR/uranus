a5bbae93c114e907bb5aa8997b5c1511a8f81fd2
28877110 <441423487@qq.com>
2019-08-15 11:14:20 +0800

    change format

--- src/main/java/com/team7/uranus/controller/OrgApplyController.java
+++ src/main/java/com/team7/uranus/controller/OrgApplyController.java
@@ -1,5 +1,6 @@
 package com.team7.uranus.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -9,8 +10,8 @@ import com.team7.uranus.entity.OrgApplyInfo;
 import com.team7.uranus.mapper.OrgApplyInfoMapper;
 import com.team7.uranus.service.OrgApplyService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.LambdaUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.NumberUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -27,7 +28,8 @@ public class OrgApplyController {
     private OrgApplyService orgApplyService;
 
     @PostMapping("/api/orgApply")
-    public ResponseData postOrgApply(@RequestBody OrgApplyInfo orgApplyInfo) {
+    public ResponseData postOrgApply(@RequestBody OrgApplyInfo orgApplyInfo,@RequestHeader(value="userId")String userId) {
+        orgApplyInfo.setApplyPerson(userId);
         orgApplyInfo.setApplyTime(LocalDateTime.now().toString());
         orgApplyInfo.setUpdateTime(LocalDateTime.now().toString());
         orgApplyInfo.setState(0);
@@ -47,7 +49,7 @@ public class OrgApplyController {
         return orgInfoResponseData;
     }
 
-    @GetMapping("/api/orgApply/{id}")
+    @GetMapping("/api/orgApply/{orgApplyId}")
     public ResponseData getChangeApply(@PathVariable int orgApplyId) {
         OrgApplyInfo orgApplyInfo = orgApplyInfoMapper.selectById(orgApplyId);
         ResponseData<OrgApplyInfo> r = new ResponseData<>();
@@ -55,7 +57,7 @@ public class OrgApplyController {
         return r;
     }
 
-    @PutMapping("/api/confirm/{orgApplyId}")
+    @PutMapping("/api/admin/confirm/{orgApplyId}")
     public ResponseData confirm(@PathVariable int orgApplyId,@RequestBody Map isAgreeBody){
         String isAgree = (String) isAgreeBody.get("isAgree");
         if(isAgree==null){
