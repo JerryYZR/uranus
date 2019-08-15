aa5dec99c19c45d88a3a8ca809c09451bd64cc97
sdz2019 <sdz2015@163.com>
2019-08-15 18:45:30 +0800

    second commit

--- src/main/java/com/team7/uranus/controller/AppInfoController.java
+++ src/main/java/com/team7/uranus/controller/AppInfoController.java
@@ -1,6 +1,9 @@
 package com.team7.uranus.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.team7.uranus.Exception.MyException;
 import com.team7.uranus.domain.ResponseData;
 import com.team7.uranus.entity.AppInfo;
@@ -17,6 +20,21 @@ public class AppInfoController {
     @Autowired
     private AppInfoMapper appInfoMapper;
 
+    //显示应用信息
+    @PostMapping("/api/getInfo")
+    public ResponseData getRep(@RequestParam int pageNum, @RequestParam String department, @RequestParam String enName) {
+        Page<AppInfo> appInfoPage = new Page<>();
+        appInfoPage.setPages(pageNum);
+        appInfoPage.setSize(10);
+        IPage<AppInfo> page = appInfoMapper.selectPage(appInfoPage,
+                new LambdaQueryWrapper<AppInfo>().like(!enName.isEmpty(), AppInfo::getEnName, enName)
+                        .eq(!department.isEmpty(), AppInfo::getDepartment, department));
+        ResponseData appInfoResponseData = new ResponseData<>();
+        appInfoResponseData.setData(page);
+        return appInfoResponseData;
+    }
+
+
 
     //增加应用信息
     @PostMapping("/api/appInfo")
@@ -29,10 +47,10 @@ public class AppInfoController {
 
     //删除应用信息
     @GetMapping("/api/appInfo/{appId}")
-    public ResponseData deleteAppInfo(@PathVariable int appId, @RequestAttribute(value = "userId") Integer appApplyId, @RequestAttribute(value = "role") Integer roles) {
+    public ResponseData deleteAppInfo(@PathVariable int appId, @RequestAttribute(value = "userId") Integer userId, @RequestAttribute(value = "roles") Integer roles) {
         AppInfo appInfo = appInfoMapper.selectById(appId);
 
-        if ((appApplyId != appInfo.getAppApplyId()) && (roles != 1)) {
+        if ((userId != appInfo.getAppApplyId()) && (roles != 1)) {
             throw new MyException(797, "该用户没有权限删除该应用");
         }
         Integer isdelete = appInfoMapper.deleteById(appId);
@@ -42,7 +60,6 @@ public class AppInfoController {
             throw new MyException(798, "删除失败");
         }
     }
-
 
     //得到应用详细信息
     @GetMapping("/api/getInfo/{appId}")
@@ -55,14 +72,9 @@ public class AppInfoController {
 
     //更新
     @PutMapping("/api/appInfo")
-    public ResponseData updateAppInfo(@RequestBody AppInfo appInfo, @RequestAttribute(value = "userId") Integer appApplyId, @RequestAttribute(value = "role") Integer roles) {
-        /*
-        appInfoMapper.updateById(appInfo);
-        return new ResponseData<>(200, "success", "success");
-    */
-        //AppInfo SelectAppInfo = appInfoMapper.selectById(appId);
+    public ResponseData updateAppInfo(@RequestBody AppInfo appInfo, @RequestAttribute(value = "userId") Integer userId, @RequestAttribute(value = "roles") Integer roles) {
 
-        if ((appInfo.getAppApplyId() != appApplyId) && (roles != 1)) {
+        if ((userId != appInfo.getAppApplyId()) && (roles != 1)) {
             throw new MyException(797, "该用户没有权限更新该应用");
         }
         appInfoMapper.updateById(appInfo);
